dab4ffdf2836d023c73d3555dda5930e4ada5308
Giperion <dereline@hotmail.com>
2021-01-17 10:25:30 +0200

    * EDIT: Redesigning build system for Win32 exclusively

---
+++ src/pch/pch.h
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstring>  // memcpy, memset
+#include <filesystem>
+#include <vector>
+#include <map>
+#include <initializer_list>
+#include <optional>
+#include <string>
+#include <type_traits>
+#include <math/constants.hpp>
+#include <math/ray.hpp>
+#include <math/vector_2d.hpp>
+#include <math/vector_3d.hpp>
+#include <math/vector_4d.hpp>
+#include <windows.h>
